14ebe67975e056b714f2399c30de664d3cfdfdf4
jainr001 <rushabh.jain@vcontractor.co.za>
2025-03-21 17:49:07 +0530

    created API to return sportsgeek backend url

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get('/sportsgeek-url', (req, res) => {
+  // res.send('Hello World!');
+  res.json({
+    name: 'SportsGeek',
+    url: 'http://65.1.72.237:8080'
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server ready on port 3000.');
+});
